3a0791c413b7abd2d2936d6ff878887a37e3bfd2
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2020-02-21 11:06:11 +0700

    update

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 echo "Soal 1 a:"
-awk -F "\t" '{arr[$13]+=$21} END{n=asort(arr,min);for(i in arr){for(a=1;a<=n;a++){if(arr[i]==min[a])ind[a]=i}}; print ind[2]}' Sample-Superstore.tsv
+reg=$(awk -F "\t" '{arr[$13]+=$21} END{n=asort(arr,min);for(i in arr){for(a=1;a<=n;a++){if(arr[i]==min[a])ind[a]=i}}; print ind[2]}' Sample-Superstore.tsv)
+echo "$reg"
 echo "Soal 1 b:"
-awk -F "\t" '$13 ~ /Central/ {arr[$11]+=$21} END{n=asort(arr,min);for(i in arr){for(a=1;a<=n;a++){if(arr[i]==min[a])ind[a]=i}}; print ind[1], ind[2]}' Sample-Superstore.tsv
-echo "soal 1 c:"
+sta=$(awk -F "\t" -v reg1=$reg '$13 ~ reg1 {arr[$11]+=$21} END{n=asort(arr,min);for(i in arr){for(a=1;a<=n;a++){if(arr[i]==min[a])ind[a]=i}}; for(c=1;c<=2;c++) print ind[c]}' Sample-Superstore.tsv)
+echo "$sta"
+echo "soal 1 c:"	
 awk -F "\t" '$11 ~ /Texas/ {arr[$17]+=$21}$11 ~ /Illinois/ {arr[$17]+=$21}END{n=asort(arr,min);for(i in arr){for(a=1;a<=n;a++){if(arr[i]==min[a])ind[a]=i}};for(i=1;i<=10;i++){print ind[i]}}' Sample-Superstore.tsv
