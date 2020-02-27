11b4f6288a3f5281e9c94beaf4efc05c3eb17532
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2020-02-27 19:58:59 +0700

    revisi

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 counterken=0
 counterdup=0
-flag=1
 for (( i=1; i<=28; i++ ))
 do
 	wget "https://loremflickr.com/320/240/cat" -O /home/iqbaal/praktikum1/bulk_gambar/pdkt_kusuma_$i.jpg 2>&1 | tee >> /home/iqbaal/praktikum1/log/wget.log
-
+	cat /home/iqbaal/praktikum1/log/wget.log | grep Location: | tail -1 | awk '{print $2}' >> /home/iqbaal/praktikum1/log/location.log
 	counterken=$(ls /home/iqbaal/kenangan/ | awk -F "_" '{print $2}'| sort -n | tail -1 | awk -F "." '{print $1}')
 	counterdup=$(ls /home/iqbaal/duplicate/ | awk -F "_" '{print $2}' | sort -n | tail -1 | awk -F "." '{print $1}')
-# echo $counterken
-
 	if [[ -z $counterken ]]
 		then
 		counterken=0
 		mv /home/iqbaal/praktikum1/bulk_gambar/pdkt_kusuma_$i.jpg /home/iqbaal/kenangan/kenangan_1.jpg
 	else
-		echo $counterken
-		for (( a=1; a<=$counterken; a++ ))
-		do	 
-			if [ diff "home/iqbaal/praktikum1/bulk_gambar/pdkt_kusuma_$i.jpg" "/home/iqbaal/kenangan/kenangan_$a.jpg" ] 
+		loc=$(cat /home/iqbaal/praktikum1/log/location.log | awk '{print $0}' | tail -1)
+		img_count=$(awk -v locate=$loc '{arr[$locate]++} END{print arr[$locate]}' /home/iqbaal/praktikum1/log/location.log)
+			if [ $img_count -gt 1 ] 
 				then
 					if [[ -z $counterdup ]]
 					then
@@ -29,7 +25,6 @@ do
 			else
 				mv /home/iqbaal/praktikum1/bulk_gambar/pdkt_kusuma_$i.jpg /home/iqbaal/kenangan/kenangan_$(($counterken+1)).jpg
 			fi
-		done
 	fi		
 done
-cp /home/iqbaal/praktikum1/log/wget.log > /home/iqbaal/praktikum1/log/wget.log.bak
+cat /home/iqbaal/praktikum1/log/wget.log > /home/iqbaal/praktikum1/log/wget.log.bak
